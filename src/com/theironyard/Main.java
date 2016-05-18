08b334febda84ec4a3ada6c30eea3d031c143686
Erik <eschneider1128@gmail.com>
2016-05-18 15:07:40 -0400

    Completed ATM with loop

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -11,41 +11,48 @@ public class Main {
         String name = scanner.nextLine();
         System.out.println("Greetings " + name);
 
-        if (name.isEmpty() ) {
+        if (name.isEmpty()) {
             throw new Exception("Invalid name.");
 
         }
-        System.out.println("What would you like to do: | 1.Check balance | 2.Withdraw funds | 3.Cancel |");
-        String choice = scanner.nextLine();
-        int numInt = Integer.valueOf(choice);
-
-        if (numInt == 1) {
-            System.out.println("Your balance is $100");
-
-        }
-        else if (numInt == 2) {
-            System.out.println("How much would you like to withdraw?");
-            String amount = scanner.nextLine();
-            int numAmount = Integer.valueOf(amount);
-
-            if (numAmount > 100) {
-                throw new Exception ("Insufficient Funds.");
-            }
+        int numInt = 0;
+         while (numInt < 3) {
 
 
-        }
-
-        else if (numInt == 3) {
-            System.out.println("Thank you please come again");
-
+             System.out.println("What would you like to do: | 1.Check balance | 2.Withdraw funds | 3.Cancel |");
+             String choice = scanner.nextLine();
+             numInt = Integer.valueOf(choice);
 
 
+             if (numInt == 1) {
+                 System.out.println("Your balance is $100");
 
-        }
+             } else if (numInt == 2) {
+                 System.out.println("How much would you like to withdraw?");
+                 String amount = scanner.nextLine();
+                 int numAmount = Integer.valueOf(amount);
+
+                 if (numAmount > 100) {
+                     throw new Exception("Insufficient Funds.");
+                 } else if (numAmount < 100) {
+                     System.out.println("Please take your money.");
+                 }
 
 
+             } else if (numInt == 3) {
+                 System.out.println("Thank you please come again.");
+
+                 break;
 
 
+             }
+             System.out.println("Would you like to complete another transaction? (Y / N)");
+             String canContinue = scanner.nextLine();
+
+             if (canContinue.equalsIgnoreCase("N") ){
+                 System.out.println("Thank you please come again.");
+                 break;
+             }
+         }
     }
-
 }
